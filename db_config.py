9aecf25deb0d1cb51c0a47e82f2abacca608d5a2
maheshv09 <vaswani.mahesh2012@gmail.com>
2024-11-15 16:42:32 +0530

    updated db config

--- db_config.py
+++ db_config.py
@@ -1,5 +1,15 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the MongoDB URI from environment variables
+MONGO_URI = os.getenv("MONGO_URI")
 
 def get_db():
-    client = MongoClient('mongodb+srv://mkv:mkv09@cluster0.kgq1cs6.mongodb.net/')
-    return client['legal_chatbot']
+    # Connect to the MongoDB database
+    client = MongoClient(MONGO_URI)
+    db = client['legal_chatbot']  # Replace 'legal_chatbot' with your database name if needed
+    return db
