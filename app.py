de4e46e0c79f31b8b9ea59bacad1c8fc1a41fbbf
maheshv09 <vaswani.mahesh2012@gmail.com>
2024-11-15 16:59:02 +0530

    updated structure

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
-from pymongo import MongoClient
 from routes import case_routes
 
 app = Flask(__name__)
 CORS(app)  # Enable Cross-Origin Resource Sharing
-
-# Initialize MongoDB
-client = MongoClient('mongodb+srv://mkv:mkv09@cluster0.kgq1cs6.mongodb.net/')
-db = client['legal_chatbot']
 
 # Register Routes
 app.register_blueprint(case_routes)
